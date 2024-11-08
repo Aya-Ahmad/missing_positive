4fcc377104b1d59add0e69016980ad6711775186
Aya Ahmad <aya.w.ahmadd@gmail.com>
2024-11-08 05:12:48 -0800

    Update missing_positive.py

--- missing_positive.py
+++ missing_positive.py
@@ -4,12 +4,14 @@ def FirstMissingPositive( numbers ):
     for i in range ( len (numbers) ):
         for j in range ( i+1 , len(numbers) ):
             if numbers [ i ] > numbers [ j ]:
-                numbers[ i ] = numbers[ j ]
-                numbers [ j ] = numbers[ i ]
+                temp = numbers [ i ]
+                numbers [ i ] = numbers [ j ]
+                numbers [ j ] = temp
 #assigning the value 1 for the first positive missing number
     missing_number = 1
-#this loop iterates over the indices of the list, checking if numbers[i] is equal to the missing number
-#if yes, it increments the missing number by 1  
+#this loop iterates over the indices of the list,
+#checking if numbers[i] is equal to the missing number
+#if yes, it increments the missing number by 1
     for i in range ( len (numbers) ):
         if numbers [ i ] == missing_number:
             missing_number += 1
